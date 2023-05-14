cc989f78125690422a213de1db003054e2826413
meowli28 <melisson.vu@gmail.com>
2023-05-14 19:43:58 +1000

    Boy1 Walks towards Target (Class)
    Created a script so boy1 would walk to front of the class.

---
+++ Assets/Characters/Boy1/CharacterController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed = 1f;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        target = GameObject.Find("Target").GetComponent<Transform>();
+    }
+
+    void Update()
+{
+    if (!isMoving)
+    {
+        isMoving = true;
+        GetComponent<Animator>().SetBool("isIdle", false);
+        GetComponent<Animator>().SetBool("isWalking", true);
+    }
+    transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+    // check if we've reached the target position
+    if (transform.position == target.position)
+    {
+        // stop moving, play idle animation
+        isMoving = false;
+        GetComponent<Animator>().SetBool("isIdle", true);
+        GetComponent<Animator>().SetBool("isWalking", false);
+    }
+}
+
+
+}
